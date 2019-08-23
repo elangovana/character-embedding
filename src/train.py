d962c312010f7f328ad69fa6569ee347bbf45ebd
aeg <aeg@amazon.com>
2019-08-23 15:57:15 +1000

    Add results writer

--- src/train.py
+++ src/train.py
@@ -3,12 +3,15 @@ import logging
 import torch
 
 from model_snapshotter import Snapshotter
+from result_writer import ResultWriter
 
 
 class Train:
 
-    def __init__(self, device=None, snapshotter=None, early_stopping=True, patience_epochs=10, epochs=10):
+    def __init__(self, device=None, snapshotter=None, early_stopping=True, patience_epochs=10, epochs=10,
+                 results_writer=None):
         # TODO: currently only single GPU
+        self.results_writer = results_writer
         self.epochs = epochs
         self.patience_epochs = patience_epochs
         self.early_stopping = early_stopping
@@ -28,6 +31,15 @@ class Train:
     def snapshotter(self, value):
         self._snapshotter = value
 
+    @property
+    def results_writer(self):
+        self._results_writer = self._results_writer or ResultWriter()
+        return self._results_writer
+
+    @results_writer.setter
+    def results_writer(self, value):
+        self._results_writer = value
+
     def run(self, train_data, val_data, model, loss_func, optimiser, output_dir):
 
         model.to(device=self.device)
@@ -35,6 +47,7 @@ class Train:
 
         patience = 0
         previous_loss = None
+        result_logs = []
         for e in range(self.epochs):
 
             n_batches = 0
@@ -92,13 +105,16 @@ class Train:
                 # Reset patience if loss decreases
                 patience = 0
 
-            print("{} {} {} {} {}".format(e, train_loss, val_loss, train_accuracy, val_accuracy))
+            print("###score### {} {} {} {} {}".format(e, train_loss, val_loss, train_accuracy, val_accuracy))
+            result_logs.append([e, train_loss, val_loss, train_accuracy.item(), val_accuracy.item()])
 
             if self.early_stopping and patience > self.patience_epochs:
                 self.logger.info("No decrease in loss for {} epochs and hence stopping".format(self.patience_epochs))
                 break
 
             previous_loss = val_loss
+
+        self.results_writer.dump_object(result_logs, output_dir, "epochs_loss")
 
     def _compute_validation_loss(self, val_data, model, loss_func):
         # Model Eval mode
@@ -120,7 +136,7 @@ class Train:
                 val_loss = loss_func(predicted_score, target)
 
                 # Total loss
-                total_loss += val_loss
+                total_loss += val_loss.item()
                 n_batches = i + 1
 
                 # Accuracy

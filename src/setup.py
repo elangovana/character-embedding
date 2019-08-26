be4d543120cb143172332d32370141c92f96d225
aeg <aeg@amazon.com>
2019-08-26 13:59:42 +1000

    rename requirements.txt

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='character-embedding',
+    version='0.1',
+    packages=['datasets'],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='Amazon Web Services',
+    author_email='',
+    description=''
+)
